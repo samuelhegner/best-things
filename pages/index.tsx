b2a8d4d4d07d862e11a430c51002a57777db3060
Yoeri Pearcey van der Beek <yoeri.pvdb@gmail.com>
2023-02-26 18:32:09 +0000

    starting frontend
    greetings,
    
    I have added a couple of initial css things to test out the layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
@@ -13,15 +14,24 @@ export default function Home() {
 				<meta name='description' content='The definitive ranking of things' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/images/favicon.ico' />
+				{/* <link rel="stylesheet" href="../styles/bt-styles.css" /> */}
 			</Head>
-			<div>
-				<h1>Official Best Things</h1>
-				<p>The creme always rises to the top</p>
-			</div>
-			<div>
-				<button type='button'>Option 1</button>
-				<button type='button'>Option 2</button>
-			</div>
+			<body>
+				<div className='container'>
+					<div>
+						<h1 id='title'>Official Best Things</h1>
+						<p>The creme always rises to the top</p>
+					</div>
+					<div>
+						<button type='button'>Option 1</button>
+						<button type='button'>Option 2</button>
+					</div>
+
+					<div className='pic'>
+						<img src='https://pbs.twimg.com/media/Dx1y9noX0AEsXcs.jpg' alt='brenny' />
+					</div>
+				</div>
+			</body>
 		</>
 	);
 }
